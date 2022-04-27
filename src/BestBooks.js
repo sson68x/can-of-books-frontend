b4021b655f36947205a779cb016b4e8686f61cfe
Seungwoo Son <sson68x@gmail.com>
2022-04-26 21:00:25 -0700

    completed lab 12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Carousel, Button } from 'react-bootstrap';
+import BookFormModal from './BookFormModal';
 
 let SERVER = process.env.REACT_APP_SERVER;
 
@@ -9,16 +10,14 @@ class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      showModal: false
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
-  
   getBooks = async () => {
     try {
       let booksUrl = `${SERVER}/books`;
-      // let booksUrl = `http://localhost:3001`;
       let booksResults = await axios.get(booksUrl);
       console.log(booksResults.data);
       this.setState({
@@ -29,48 +28,111 @@ class BestBooks extends React.Component {
     }
   }
 
+  postBook = async (book) => {
+    try {
+      let booksUrl = `${SERVER}/books`;
+      let addedBook = await axios.post(booksUrl, book);
+      console.log(addedBook.data);
+      this.setState({
+        books: [...this.state.books, addedBook.data]
+      });
+    } catch (error) {
+      console.log('We have an error: ', error.response.data);
+    }
+  }
+
+  deleteBook = async (id) => {
+    try {
+      let booksUrl = `${SERVER}/books/${id}`;
+      await axios.delete(booksUrl);
+      let updatedBooks = this.state.books.filter(book => book._id !== id);
+      this.setState({
+        books: updatedBooks
+      })
+    } catch (error) {
+      console.log(`We have an error:`)
+    }
+  }
+
+  handleBookSubmit = (e) => {
+    e.preventDefault();
+    let book = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      author: e.target.author.value,
+      status: e.target.status.value
+    }
+    this.postBook(book);
+  }
+
   componentDidMount() {
     this.getBooks();
   }
 
+  hideModal = () => {
+    this.setState({
+      showAddModal: false
+    })
+  }
+
+  showAddModal = () => {
+    this.setState({
+      showAddModal: true,
+    })
+  }
 
   render() {
 
-    /* TODO: render all the books in a Carousel */
-
     return (
-
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <main>
+          <Carousel>
+            {this.state.books.length > 0 ? (
+              this.state.books.map((book) => {
+                return (
+                  <Carousel.Item key={book._id}>
+                    <img
+                      className="d-block w-100"
+                      src="https://place-hold.it/800x400/000"
+                      alt="Best Books"
+                    />
+                    <Carousel.Caption>
+                      <h3 style={{
+                        backgroundColor: 'lightblue',
+                        borderRadius: '5px',
+                        width: 'max-content',
+                        margin: 'auto'
+                      }}>
+                        {book.title} by {book.author}
+                      </h3>
+                      <Button
+                      style={{
+                        margin: '10px'
+                      }}
+                        onClick={this.showAddModal}
+                        type="submit" variant="primary">Add Book
+                      </Button>
+                      <Button onClick={() => this.deleteBook(book._id)}
+                        type="submit" variant="secondary">Delete Book
+                      </Button>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                );
+              })
+            )
+              : (
+                <h3>No Books Found</h3>
+              )}
+          </Carousel>
 
-        <Carousel>
-          {this.state.books.length > 0 ? (
-            this.state.books.map((book) => {
-              return (
-                <Carousel.Item key={book._id}>
-                  <img
-                    className="d-block w-100"
-                    src="https://place-hold.it/800x400/000"
-                    alt="Best Books"
-                  />
-                  <Carousel.Caption>
-                    <h3 style={{
-                      backgroundColor: 'lightblue',
-                      borderRadius: '5px',
-                      width: 'max-content',
-                      margin: 'auto'
-                    }}>
-                      {book.title} by {book.author}
-                    </h3>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              );
-            })
-          )
-            : (
-              <h3>No Books Found</h3>
-            )}
-        </Carousel>
+          <BookFormModal
+            showAddModal={this.state.showAddModal}
+            // showDeleteModal={this.state.showDeleteModal}
+            hideModal={this.hideModal}
+            handleBookSubmit={this.handleBookSubmit}
+          />
+        </main>
       </>
     )
   }
